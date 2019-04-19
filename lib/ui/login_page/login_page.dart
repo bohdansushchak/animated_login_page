504578b3c68d3ff0bf721c7753c71c278d65007d
Bohdan Sushchak <bogdansushak@gmail.com>
2019-04-19 15:24:44 +0200

    Implemented a teddy controller and design of login page

---
+++ lib/ui/login_page/login_page.dart
@@ -0,0 +1,117 @@
+import 'package:animated_login_page/ui/login_page/teddy_controller.dart';
+import 'package:animated_login_page/ui/widget/signin_button.dart';
+import 'package:animated_login_page/ui/widget/tracking_text_input.dart';
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPage createState() => _LoginPage();
+}
+
+class _LoginPage extends State<LoginPage> {
+  TeddyController _teddyController;
+
+  @override
+  initState() {
+    _teddyController = TeddyController();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _buildScaffold();
+  }
+
+  Scaffold _buildScaffold() {
+    EdgeInsets devicePadding = MediaQuery.of(context).padding;
+
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(93, 142, 155, 1.0),
+      body: Container(
+          child: Stack(
+        children: <Widget>[
+          Positioned.fill(
+              child: Container(
+            decoration: BoxDecoration(
+              // Box decoration takes a gradient
+              gradient: LinearGradient(
+                // Where the linear gradient begins and ends
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                // Add one stop for each color. Stops should increase from 0 to 1
+                stops: [0.0, 1.0],
+                colors: [
+                  Color.fromRGBO(170, 207, 211, 1.0),
+                  Color.fromRGBO(93, 142, 155, 1.0),
+                ],
+              ),
+            ),
+          )),
+          Positioned.fill(
+            child: SingleChildScrollView(
+                padding: EdgeInsets.only(
+                    left: 20.0, right: 20.0, top: devicePadding.top + 50.0),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Container(
+                          height: 200,
+                          padding:
+                              const EdgeInsets.only(left: 30.0, right: 30.0),
+                          child: FlareActor(
+                            "assets/teddy.flr",
+                            shouldClip: false,
+                            alignment: Alignment.bottomCenter,
+                            fit: BoxFit.contain,
+                            controller: _teddyController,
+                          )),
+                      Container(
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(25.0))),
+                          child: Padding(
+                            padding: const EdgeInsets.all(30.0),
+                            child: Form(
+                                child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                TrackingTextInput(
+                                    label: "Email",
+                                    hint: "What's your email address?",
+                                    onCaretMoved: (Offset caret) {
+                                      _teddyController.lookAt(caret);
+                                    }),
+                                TrackingTextInput(
+                                  label: "Password",
+                                  hint: "Try 'bears'...",
+                                  isObscured: true,
+                                  onCaretMoved: (Offset caret) {
+                                    _teddyController.coverEyes(caret != null);
+                                    _teddyController.lookAt(null);
+                                  },
+                                  onTextChanged: (String value) {
+                                    _teddyController.setPassword(value);
+                                  },
+                                ),
+                                SigninButton(
+                                    child: Text("Sign In",
+                                        style: TextStyle(
+                                            fontSize: 16,
+                                            color: Colors.white)),
+                                    onPressed: () {
+                                      _teddyController.submitPassword();
+                                    })
+                              ],
+                            )),
+                          )),
+                    ])),
+          ),
+        ],
+      )),
+    );
+  }
+}
